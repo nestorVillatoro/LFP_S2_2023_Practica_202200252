978bd067da820e3b99ba2afa0ff4cee2b25e0a20
nestorVillatoro <124467934+nestorVillatoro@users.noreply.github.com>
2023-08-22 16:08:48 -0600

    Practica 1

--- practica1_LFP/menu.py
+++ practica1_LFP/menu.py
@@ -6,75 +6,173 @@ cantidades =[]
 precios = []
 ubicaciones = []
 def cargarInvIn():
-    print("-----------Cargar Inventario Inicial-----------")
-    nombre = input("Ingrese el nombre del producto: ")
-    nombres.append(nombre)
-    cantidad = int(input("ingrese la cantidad: "))
-    cantidades.append(cantidad)
-    precio = float(input("ingrese el precio: "))
-    precios.append(precio)
-    ubicacion = input("ingrese la ubicacion: ")
-    ubicaciones.append(ubicacion)
-    archivo = open('inventario.inv', 'a+')
-    archivo.write("\ncrear_producto " + nombre + "; " + str(cantidad) + "; " + str(precio) + "; " + ubicacion + " ")
-    archivo.seek(0)
-    var_inv = archivo.read()
-    print("")
-    print(var_inv)
-    print("")
-    archivo.close()
+    print("-----------Cargar inventario inicial-----------")
+    print("Que desea hacer?")
+    print("1. Exportar")
+    print("2. Agregar manualmente")
+    op = int(input("Ingrese una opción: "))
+    match op:
+        case 1:
+            ruta = input("Ingrese la ruta del archivo: ")
+            archivo = open(ruta, "r", encoding="utf-8")
+
+            for linea in archivo.readlines():
+                remp = linea.split(" ")
+                items = remp[1].split(";")
+                nombres.append(items[0])
+                cantidades.append(int(items[1]))
+                precios.append(float(items[2]))
+                ubicaciones.append(items[3].strip("\n"))
+
+            archivo.close()
+
+            print("Nombres:", nombres)
+            print("Cantidades:", cantidades)
+            print("Precios:", precios)
+            print("Ubicaciones:", ubicaciones)
+        case 2:
+            print("-----------Cargar inventario inicial-----------")
+            nombre = input("Ingrese el nombre del producto: ")
+            cantidad = int(input("ingrese la cantidad: "))
+            precio = float(input("ingrese el precio: "))
+            ubicacion = input("ingrese la ubicacion: ")
+
+            for i in range(len(nombres)):
+                if nombre == nombres[i] & ubicacion == ubicaciones[i]:
+                    print("el producto ya existe en esta ubicacion")
+                    menu()
+
+            nombres.append(nombre)
+            cantidades.append(cantidad)
+            precios.append(precio)
+            ubicaciones.append(ubicacion)
+            archivo = open('inventario.inv', 'a+')
+            archivo.write("\ncrear_producto " + nombre + "; " + str(cantidad) + "; " + str(precio) + "; " + ubicacion + " ")
+            archivo.seek(0)
+            var_inv = archivo.read()
+            print("")
+            print(var_inv)
+            print("")
+            archivo.close()
+        case _:
+            print("Opción no válida")
+            menu()
     input("Presione una tecla para continuar...")
 
 def cargarInsMov():
     print("-----------Cargar Instrucciones de Movimientos-----------")
     print("Que desea hacer?")
-    print("1. Agregar stock")
-    print("2. Vender producto")
-    op = int(input("Ingrese una opción: "))
-    match op:
-        case 1:
-            nombre = input("Ingrese el nombre del producto que desea agregar: ")
-            cantidad = int(input("Ingrese la cantidad que desea agregar: "))
-            ubicacion = input("Ingrese la ubicacion donde desea agregar: ")
-            for i in range(len(nombres)):
-                if nombre == nombres[i]:
-                    if ubicacion == ubicaciones[i]:
-                        cantidades[i] = cantidades[i] + cantidad
-                        archivo = open('movimientos.mov', 'a+')
-                        archivo.write("\nagregar_stock " + nombre + "; " + str(cantidad) + "; " + ubicacion + " ")
-                        archivo.seek(0)
-                        var_mov = archivo.read()
-                        print("")
-                        print(var_mov)
-                        print("")
-                        archivo.close()
-                    else:
-                        print("El producto no existe en esa ubicacion!")
-            
+    print("1. Exportar")
+    print("2. Cargar manualmente")
+    ops = int(input("Ingrese una opción: "))
+    match ops:
+        case 1: 
+            ruta = input("Ingrese la ruta del archivo: ")
+            archivo = open(ruta, "r", encoding="utf-8")
+
+            for linea in archivo.readlines():
+                remp = linea.strip().split(";")
+                items = remp[0].split(" ")
+                accion = items[0]
+                nombre = items[1]
+                cantidad = int(remp[1])
+                ubicacion = remp[2]
+                if accion == "agregar_stock":
+                    com = False
+                    for i in range(len(ubicaciones)):
+                        if ubicacion == ubicaciones[i] and nombre == nombres[i]:
+                                cantidades[i] = cantidades[i] + cantidad
+                                com = True
+                                break
+                    if com == False:
+                        print("Producto o Ubicación no encontrados!")
+
+                elif accion == "vender_producto":
+                    com = False
+                    for i in range(len(ubicaciones)):
+                        if ubicacion == ubicaciones[i] and nombre == nombres[i]:
+                                if cantidad <= cantidades[i]:
+                                    cantidades[i] = cantidades[i] - cantidad
+                                else:
+                                    print("La cantidad a vender no puede ser mayor al stock!")
+                                com = True
+                                break
+                        
+                    if com == False:
+                        print("Producto o Ubicación no encontrados!")
+
+                else:
+                    break
+                
+            archivo.close()
+
+            print("Nombres:", nombres)
+            print("Cantidades:", cantidades)
+            print("Precios:", precios)
+            print("Ubicaciones:", ubicaciones)
         case 2:
-            nombre = input("Ingrese el nombre del producto que desea vender: ")
-            cantidad = int(input("Ingrese la cantidad que desea vender: "))
-            ubicacion = input("Ingrese la ubicacion donde desea vender: ")
-            for i in range(len(nombres)):
-                if nombre == nombres[i]:
-                    if ubicacion == ubicaciones[i]:
-                        if cantidad<cantidades[i]:
-                            cantidades[i] = cantidades[i] - cantidad
-                            archivo = open('movimientos.mov', 'a+')
-                            archivo.write("\nvender_producto " + nombre + "; " + str(cantidad) + "; " + ubicacion + " ")
-                            archivo.seek(0)
-                            var_mov = archivo.read()
-                            print("")
-                            print(var_mov)
-                            print("")
-                            archivo.close()
-                        else:
-                            print("No hay suficientes existencias!")
-                    else:
-                        print("El producto no existe en esa ubicacion!")
+            print("-----------Cargar Instrucciones de Movimientos-----------")
+            print("Que desea hacer?")
+            print("1. Agregar stock")
+            print("2. Vender producto")
+            op = int(input("Ingrese una opción: "))
+            match op:
+                case 1:
+                    nombre = input("Ingrese el nombre del producto que desea agregar: ")
+                    cantidad = int(input("Ingrese la cantidad que desea agregar: "))
+                    ubicacion = input("Ingrese la ubicacion donde desea agregar: ")
+                    com = False
+                    for i in range(len(ubicaciones)):
+                        if ubicacion == ubicaciones[i] and nombre == nombres[i]:
+                                cantidades[i] = cantidades[i] + cantidad
+                                com = True
+                                break
+                        
+                    if com == False:
+                        print("Producto o Ubicación no encontrados!")
+                        cargarInsMov()
+
+                    archivo = open('movimientos.mov', 'a+')
+                    archivo.write("\nagregar_stock " + nombre + "; " + str(cantidad) + "; " + ubicacion + " ")
+                    archivo.seek(0)
+                    var_mov = archivo.read()
+                    print("")
+                    print(var_mov)
+                    print("")
+                    archivo.close()
+        
+                case 2:
+                    nombre = input("Ingrese el nombre del producto que desea vender: ")
+                    cantidad = int(input("Ingrese la cantidad que desea vender: "))
+                    ubicacion = input("Ingrese la ubicacion donde desea vender: ")
+                    com = False
+                    for i in range(len(ubicaciones)):
+                        if ubicacion == ubicaciones[i] and nombre == nombres[i]:
+                                if cantidad <= cantidades[i]:
+                                    cantidades[i] = cantidades[i] - cantidad
+                                else:
+                                    print("La cantidad a vender no puede ser mayor al stock!")
+                                    cargarInsMov()
+                                com = True
+                                break
+                        
+                    if com == False:
+                        print("Producto o Ubicación no encontrados!")
+                        cargarInsMov()
+
+                    archivo = open('movimientos.mov', 'a+')
+                    archivo.write("\nagregar_stock " + nombre + "; " + str(cantidad) + "; " + ubicacion + " ")
+                    archivo.seek(0)
+                    var_mov = archivo.read()
+                    print("")
+                    print(var_mov)
+                    print("")
+                    archivo.close()
+                    print("Opción no válida")
+                    cargarInsMov()
         case _:
-            print("Opción no válida")
-            cargarInsMov()
+            print("Opcion no valida")
+            menu()
     input("Presione una tecla para continuar...")
 
 def crearInfoInv():
